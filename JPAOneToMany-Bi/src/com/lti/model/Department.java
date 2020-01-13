28848df678e311f8dd351566264c29219f802580
Anukool Srivastava <anukoolsriv@gmail.com>
2020-01-14 01:01:31 +0530

    Add files via upload
    attempted 5 queries given - not completed

--- JPAOneToMany-Bi/src/com/lti/model/Department.java
+++ JPAOneToMany-Bi/src/com/lti/model/Department.java
@@ -4,7 +4,14 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Table(name = "dept")
 @Entity
